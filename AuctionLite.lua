73552acc1850b302b9a16cd53ea20cbc27377090
merialkilrogg <merialkilrogg@906b8af0-bf8a-4471-b169-cf5549599b2f>
2009-05-22 03:22:32 +0000

    Bump version number to 1.2.1.

--- AuctionLite.lua
+++ AuctionLite.lua
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- AuctionLite 1.2.0
+-- AuctionLite 1.2.1
 --
 -- Lightweight addon to determine accurate market prices and to simplify
 -- the process of posting auctions.
@@ -292,7 +292,7 @@ local Defaults = {
 
 local DBName = "AuctionLiteDB";
 
-local AUCTIONLITE_VERSION = "1.2.0";
+local AUCTIONLITE_VERSION = "1.2.1";
 
 -------------------------------------------------------------------------------
 -- Hooks and boostrap code
